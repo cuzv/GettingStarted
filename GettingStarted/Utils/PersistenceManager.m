714b2668a361954a294a81d6fd57f09c004816b2
atcuan <atcuan@gmail.com>
2014-09-03 23:13:04 +0800

    can add data for PersistenceManager

--- GettingStarted/Utils/PersistenceManager.m
+++ GettingStarted/Utils/PersistenceManager.m
@@ -14,7 +14,7 @@
 + (BOOL)persistenceValue:(id)value forKey:(NSString *)key {
     if (!value) {
         // Remove value for key
-        value = [NSNull null];
+        return [self cleanPersistenceValueForKey:key];
     }
     // Add value for key
     // Create data container
@@ -24,16 +24,25 @@
     [archiver encodeObject:value forKey:key];
     // This step is very import
     [archiver finishEncoding];
-
+    
     // convert to base64
     NSString *base64String = [archiverMutableData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
-
+    
     NSError *error = nil;
-    return [base64String writeToFile:[self persistencePath] atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    return [base64String writeToFile:[self persistencePathWithKey:key] atomically:YES encoding:NSUTF8StringEncoding error:&error];
+}
+
++ (NSString *)persistencePathWithKey:(NSString *)key {
+    return [[self persistencePath] stringByAppendingPathComponent:key];
 }
 
 + (NSString *)persistencePath {
-    return [[self applicationDocumentDirectory] stringByAppendingPathComponent:@"PersistenceDatas"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *path = [[self applicationDocumentDirectory] stringByAppendingPathComponent:@"PersistenceDatas"];
+    if (![fileManager fileExistsAtPath:path]) {
+        [fileManager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
+    };
+    return path;
 }
 
 + (NSString *)applicationDocumentDirectory {
@@ -42,20 +51,28 @@
 
 + (id)persistenceValueForKey:(NSString *)key {
     NSError *error = nil;
-    NSString *unarchiverString = [[NSString alloc] initWithContentsOfFile:[self persistencePath] encoding:NSUTF8StringEncoding error:&error];
+    NSString *unarchiverString = [[NSString alloc] initWithContentsOfFile:[self persistencePathWithKey:key] encoding:NSUTF8StringEncoding error:&error];
     NSData *unarchiverData = [[NSData alloc] initWithBase64EncodedString:unarchiverString options:NSDataBase64DecodingIgnoreUnknownCharacters];
-
+    
     // Connect unarchiver data and unarchiver
     NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:unarchiverData];
-
+    
     return [unarchiver decodeObjectForKey:key];
 }
 
++ (BOOL)cleanPersistenceValueForKey:(NSString *)key {
+    return [self cleanupForPath:[self persistencePathWithKey:key]];
+}
+
 + (BOOL)cleanup {
+    return [self cleanupForPath:[self persistencePath]];
+}
+
++ (BOOL)cleanupForPath:(NSString *)path {
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    if ([fileManager isDeletableFileAtPath:[self persistencePath]]) {
+    if ([fileManager isDeletableFileAtPath:path]) {
         NSError *error = nil;
-        BOOL success = [fileManager removeItemAtPath:[self persistencePath] error:&error];
+        BOOL success = [fileManager removeItemAtPath:path error:&error];
         return success;
     };
     
