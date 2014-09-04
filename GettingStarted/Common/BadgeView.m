2bb1a5f82d58729c652e268ed1f1f7dccee7a726
atcuan <atcuan@gmail.com>
2014-09-04 20:46:18 +0800

    M BadgeView

--- GettingStarted/Common/BadgeView.m
+++ GettingStarted/Common/BadgeView.m
@@ -52,16 +52,16 @@
                                            context:nil].size.width + 4;
     self.width = self.width < 20 ? 20 : self.width;
     self.height = 20.0f;
-    if(unreadNumber > 0){
+    if(unreadNumber > 0) {
         [self setNeedsDisplay];
         self.hidden = NO;
-    }else{
+    } else {
         self.hidden = YES;
     }
 }
 
 - (void)drawRect:(CGRect)rect {
-    NSString *unreadStr = _unreadNumber > 99 ? [NSString stringWithFormat:@"99+"] : [NSString stringWithFormat:@"%d",_unreadNumber];
+    NSString *unreadStr = _unreadNumber > 99 ? [NSString stringWithFormat:@"99+"] : [NSString stringWithFormat:@"%d", _unreadNumber];
     _badgeLabel.text = unreadStr;
     self.layer.cornerRadius = self.width > self.height + 5 ? self.width / 3 : self.width / 2;
     [_badgeLabel drawTextInRect:self.bounds];
--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 platform :ios,'7.0'
 pod 'AFNetworking', '~> 2.3.1'
-pod 'RefreshControl'
+pod 'RefreshControl', :head
 pod 'UIActionSheet+Blocks'
 pod 'UIAlertView+Blocks'
 pod 'JGProgressHUD'
