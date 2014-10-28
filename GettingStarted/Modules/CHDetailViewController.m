3d886d9c676d4a13620e9ab7c630f4aa02c23fe1
atcuan <atcuan@gmail.com>
2014-10-28 15:57:12 +0800

    add arc view

--- GettingStarted/Modules/CHDetailViewController.m
+++ GettingStarted/Modules/CHDetailViewController.m
@@ -51,16 +51,16 @@
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    NSDictionary *dict = @{@"cid":@"1001",
-//                           @"oid":@"6611"};
-//    [HTTPManager setHTTPRequestSerializerType:HTTPRequestSerializerTypeBase64];
-//    [HTTPManager requestWillBeginWithProgressAnimation];
-//    [HTTPManager POSTWithMethodName:[NSString stringWithFormat:@"v4_%@.do", kMessageList] parameters:dict success:^(NSURLSessionDataTask *task, id responseObject) {
-//        NSLog(@"%@", responseObject);
-//    } failure:^(NSURLSessionDataTask *task, NSError *error) {
-//        NSLog(@"%@", error);
-//    }];
-//
+    NSDictionary *dict = @{@"cid":@"1001",
+                           @"oid":@"6611"};
+    [HTTPManager setHTTPRequestSerializerType:HTTPRequestSerializerTypeBase64];
+    [HTTPManager requestWillBeginWithProgressAnimation];
+    [HTTPManager POSTWithMethodName:[NSString stringWithFormat:@"v4_%@.do", kMessageList] parameters:dict success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSLog(@"%@", responseObject);
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"%@", error);
+    }];
+
 
 //    [UIView toastWithMessage:@"密码错误"];
     
