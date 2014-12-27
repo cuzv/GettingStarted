37aad925a3cd9c33a546de689fafd868b68fea1e
atcuan <atcuan@gmail.com>
2014-12-27 17:57:36 +0800

    add icon; download file; cache response object; CFnetworkError print

---
+++ GettingStarted/Request/CHXDownloadRequest.h
@@ -0,0 +1,13 @@
+//
+//  CHXDownloadRequest.h
+//  GettingStarted
+//
+//  Created by Moch Xiao on 12/27/14.
+//  Copyright (c) 2014 Foobar. All rights reserved.
+//
+
+#import "CHXRequest.h"
+
+@interface CHXDownloadRequest : CHXRequest
+
+@end
---
+++ GettingStarted/Request/CHXDownloadRequest.m
@@ -0,0 +1,13 @@
+//
+//  CHXDownloadRequest.m
+//  GettingStarted
+//
+//  Created by Moch Xiao on 12/27/14.
+//  Copyright (c) 2014 Foobar. All rights reserved.
+//
+
+#import "CHXDownloadRequest.h"
+
+@implementation CHXDownloadRequest
+
+@end
